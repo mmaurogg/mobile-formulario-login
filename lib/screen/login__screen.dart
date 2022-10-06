b622b6b6086813cd97e38ee480b024ee8dc5e3e6
mmaurogg <mmaurogg@gmail.com>
2022-10-06 00:48:27 -0500

    agregado cuerpo del login

--- lib/screen/login__screen.dart
+++ lib/screen/login__screen.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:formularios_app/widgets/widgets.dart';
+import 'package:formularios_app/ui/input_decorations.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,11 +11,88 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: AuthBackground(
-        child: Container(
-          width: double.infinity,
-          height: 300,
-          color: Colors.red,
-        ),)
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 250,),
+
+              CardContainer(
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    Text('Login', style: Theme.of(context).textTheme.headline4,),
+
+                    SizedBox(height: 30),
+                    _LoginForm(),
+
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 50),
+              Text('Crear una nueva cuenta', style: TextStyle( fontWeight: FontWeight.bold, fontSize: 18),),
+              SizedBox(height: 500),
+
+
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        //TODO mantener la referencia aqui
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'JhonDoe@algo.com',
+                labelText: 'Correo electrónico',
+                prefixIcon: Icons.alternate_email_rounded
+              ),
+            ),
+            SizedBox(height: 30),
+
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '******',
+                  labelText: 'Contraseña',
+                  prefixIcon: Icons.lock_outline
+              ),
+            ),
+            SizedBox(height: 30),
+            
+            MaterialButton(
+              shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: Text(
+                  'Ingresar',
+                  style: TextStyle( color: Colors.white)
+                ),
+              ),
+              onPressed: () {
+                //TODO: login
+            },)
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,6 @@
-
+export 'package:formularios_app/widgets/card_container.dart';
 export 'package:formularios_app/widgets/auth_background.dart';
+
+
+
+
